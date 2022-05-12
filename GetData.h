10d18485cd6cc1c42a18257883a2c7f45497bcb0
Asahi <82006976+toasahi@users.noreply.github.com>
2022-05-12 10:44:56 +0900

    Update GetData.h

--- GetData.h
+++ GetData.h
@@ -1,13 +1,4 @@
-/*
- * 土の水分量を測定する
- * int soilPin 制御ピン
- * return moist 水分量
- */
-
-int readSoil(int soilPin) {
-  delay(10);
-  int val = analogRead(soilPin);
-  int moist = map(val, 0, 4095, 0, 99);
-  moist = 100 - moist;
-  return moist;
-}
+int readSoil(int soilPin);
+byte getTemerature();
+byte getHumidity();
+uint16_t getAirPressure();
