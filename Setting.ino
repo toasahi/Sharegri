b0e067a2e244f22d9c997db911ccd0d5072573a2
Asahi <82006976+toasahi@users.noreply.github.com>
2022-05-12 10:12:44 +0900

    Update Setting.ino
    ESP32を識別するIDを取得

--- Setting.ino
+++ Setting.ino
@@ -12,3 +12,12 @@ unsigned long getTime(){
   unsigned long currentTime = tm->tm_hour;
   return currentTime;
 }
+
+
+uint32_t getChipId(){
+  uint32_t chipId = 0;
+  for(int i=0; i<17; i=i+8) {
+    chipId |= ((ESP.getEfuseMac() >> (40 - i)) & 0xff) << i;
+  }
+  return chipId;
+}
